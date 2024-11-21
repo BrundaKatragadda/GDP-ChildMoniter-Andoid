2f4ae70455e8fe62c02308d9cc99df4a23d048b0
SRI SOWMYA POLAMPALLI <156372013+srisowmyagoud@users.noreply.github.com>
2024-11-20 20:18:48 -0600

    Added login and Email user information details

--- app/src/main/java/com/example/childmonitoringapp/MainActivity.java
+++ app/src/main/java/com/example/childmonitoringapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.childmonitoringapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,18 +10,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    Button Button;
+    TextView textView;
+    FirebaseUser user;
     @Override
     
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        auth = FirebaseAuth.getInstance();
+        Button = findViewById(R.id.logout);
+        textView =findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+        if(user== null){
+            Intent intent=new Intent(getApplicationContext().Login.class);
+            startActivity(intent);
+            finish();
+        }else{
+            textView.setText(user.getEmail());
+        }
+
     }
 }
